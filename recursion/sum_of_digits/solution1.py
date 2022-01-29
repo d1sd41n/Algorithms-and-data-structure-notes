00c1bd21cfd2d37df9b946781b937f444ce9c3db
d1sd41n <outnumbered@live.com>
2022-01-29 13:22:26 -0500

    sum digits

--- recursion/sum_of_digits/solution1.py
+++ recursion/sum_of_digits/solution1.py
@@ -1,3 +1,13 @@
 
-def sum_(x1, x2):
-    pass
+def sum_(x):
+    
+    assert int(x) == x and x > 0, "error, bad input"
+    if x < 10:
+        return x
+
+    last_digit = x % 10
+    cut_number = (x/10) - (last_digit/10)
+
+    return last_digit + sum_(cut_number)
+
+print(sum_(1234))
