6478991c34aee29927e2ecfe7e2974ab6ef62039
d1sd41n <outnumbered@live.com>
2022-03-19 01:08:12 -0500

    nestedEvenSum

--- recursion/nestedEvenSum/soution1.py
+++ recursion/nestedEvenSum/soution1.py
@@ -1,5 +1,15 @@
 def nestedEvenSum(obj, sum=0):
-    pass
+    # import pdb; pdb.set_trace()
+
+    for i in obj:
+      if type(obj[i]) in (int, float):
+        if obj[i] % 2 == 0:
+          sum += obj[i]
+      
+      elif type(obj[i]) == dict:
+        sum += nestedEvenSum(obj[i], sum=0)
+    
+    return sum
 
 obj1 = {
   "outer": 2,
@@ -13,6 +23,8 @@ obj1 = {
   }
 }
 
+# print(nestedEvenSum(obj1))
+
 assert nestedEvenSum(obj1) == 6
 
 obj2 = {
@@ -24,3 +36,16 @@ obj2 = {
 }
 
 assert nestedEvenSum(obj2) == 10
+
+
+obj2 = {
+  "a": 2,
+  "b": {"b": 2, "bb": {"b": 3, "bb": {"b": 2}}},
+  "c": {"c": {"c": 2}, "cc": 'ball', "ccc": 5},
+  "d": 1,
+  "d": 4,
+  "d2": 43,
+  "e": {"e": {"e": 2, "ww": {"w": 22}}, "ee": 'car'}
+}
+
+assert nestedEvenSum(obj2) == 14 +22
